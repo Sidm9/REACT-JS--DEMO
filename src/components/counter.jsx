4e832d768618d089f6950b0b6bead494af9b5314
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-07-18 20:52:17 +0530

    STABLE FORMATTED DONE

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,27 +4,28 @@ import { returnStatement } from "@babel/types";
 
 class Counter extends React.Component {
   state = {
-    count: 0 ,
+    count: 0
     //imageURL: "https:/picsum.photos/200"
   };
-   styles = {
-     fontSize: 10,
-     fontWeight: 'bold'
-   };
-
   render() {
     return (
       <React.Fragment>
         <div>
-          <span style={ this.styles }className = "badge badge-primary m-2"> {this.formatCount()}</span> 
-          <button className = "btn btn-secondary btn-sm"> Increment </button>
+          <span
+            style={{ fontSize: 10, fontWeight: "bold" }}
+            className="badge badge-primary m-2" //CSS?!?
+          >
+            {" "}
+            {this.formatCount()}
+          </span>
+          <button className="btn btn-secondary btn-sm"> Increment </button>
         </div>
       </React.Fragment>
     );
   }
 
   formatCount() {
-    const { count } = this.state;  //DESTRUCTOR
+    const { count } = this.state; //DESTRUCTOR
     return count === 0 ? "Zero" : count;
   }
 }
