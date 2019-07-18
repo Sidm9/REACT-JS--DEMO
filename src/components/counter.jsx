4093a3877b3964b7ef5595a5aa0cadf323908803
SidM9 <43111742+Sidm9@users.noreply.github.com>
2019-07-18 23:48:00 +0530

    TAGS EMPTY MESSAGE

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,36 +4,29 @@ import { returnStatement } from "@babel/types";
 
 class Counter extends React.Component {
   state = {
-    count: 5
+    count: 5,
+    tag: []
     //imageURL: "https:/picsum.photos/200"
   };
+
+  renderTags() {
+    if (this.state.tag.length === 0) return <p>THERE ARE NO TAGS</p>;
+    return (
+      <ul>
+        <li>
+          {this.state.tag.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </li>
+      </ul>
+    );
+  }
   render() {
     return (
       <React.Fragment>
-        <div>
-          <span
-            className={this.getBadgeClasses()}
-            style={{ fontSize: 10, fontWeight: "bold" }}
-          >
-            {this.formatCount()}
-          </span>
-          <button className="btn btn-secondary btn-sm"> Increment </button>
-        </div>
+        <div>{this.renderTags()}</div>
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-"; //HERE M-2 IS FOR MARGIN AND BADGE IS SOME CSS ...SORT OF
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-    /* CONDITIONAL EXPRESSION TO CHECK IF THE 
-    /*VALUE IS ZERO THEN WARNING  (THIS WILL CHANGE COLOUR TO YELLOW ELSE BLUE)   */
-  }
-
-  formatCount() {
-    const { count } = this.state; //DESTRUCTOR
-    return count === 0 ? "Zero" : count;
   }
 }
 export default Counter;
